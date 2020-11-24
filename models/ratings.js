26493b6aa76bd95e8f7b62b40bbc5a6d17aa3320
Nick Smolen <nasmolen@gmail.com>
2020-11-24 11:08:54 -0500

    first rating push

--- models/ratings.js
+++ models/ratings.js
@@ -4,19 +4,23 @@ const db = require('../db');
 const Rating = db.define('rating', {
     rating: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            min: 0,
+            max: 10
+          }
     },
     movieId: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: true
     },
     userId: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: true
     },
     genresId: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: true
     }
 })
 
